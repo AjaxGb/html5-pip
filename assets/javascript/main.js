aba671628da45c3233373c659cc645c3c37dc168
dawsoju <justin@justindawson.com>
2013-07-07 22:49:51 -0700

    Remarks

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,12 @@
 var angryBirds = document.getElementById('angryBirdsVideo');
 
-
+//  Change Angry birds to PUP
 function doPictureInPicture(){
  angryBirds.setAttribute('class', 'pip');
  console.log('Angry Birds are now PIP');
-}
+} // doPictureInPicture()
 
 function stopPictureInPicture(){
   angryBirds.removeAttribute('class');
   console.log('Angry Birds are now Full Screen');
-}
+} // stopPictureInPicture()
