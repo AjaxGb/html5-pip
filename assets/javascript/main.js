e369642fd8ce1398ff657f3d20d1801c9a3b96bd
dawsoju <justin@justindawson.com>
2013-07-09 17:16:13 -0700

    added screen shot

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -81,3 +81,31 @@ var dzs = document.querySelectorAll('#dropZones .upperRight');
 
 
 
+//Screenshot
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
+var w, h, ratio;
+
+video.addEventListener('loadedmetadata', function(){
+	ratio = video.videoWidth / video.videoHeight;
+	w = video.videoWidth - 100;
+	h = parseInt(w / ratio, 10);
+	canvas.width = w;
+	canvas.height = h;
+}, false);
+
+function snap(e){
+	context.fillRect(0, 0, w, h);
+	context.drawImage(video, 0, 0, w, h);
+	console.log('Cap the screen');
+}
+
+
+
+
+
+
+
+
+
